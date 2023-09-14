efd08703267b16a3ad7e9992554cbb820df295b9
John Ault <john.ault.dev@gmail.com>
2023-09-14 09:32:49 -0400

    quick formatting

--- src/api/weatherapi.js
+++ src/api/weatherapi.js
@@ -3,28 +3,28 @@ import axios from 'axios';
 const baseUrl = 'https://api.tomorrow.io/v4/weather/forecast?';
 
 export const getWeatherData = async (cityname) => {
-    try{
+    try {
         const {data} = await axios.get(baseUrl + `apikey=4fL4g5wwRWiCXgM9cIQQncjVY9yFv9No&location=${cityname}`)
         return data;
         console.table(data);
-    }catch(error) {
+    } catch(error) {
         if (error.response) {
             // The request was made and the server responded with a status code
             // that falls out of the range of 2xx
             console.log(error.response.data);
             console.log(error.response.status);
             console.log(error.response.headers);
-            } else if (error.request) {
+        } else if (error.request) {
             // The request was made but no response was received
             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
             // http.ClientRequest in node.js
             console.log(error.request);
-            } else {
+        } else {
             // Something happened in setting up the request that triggered an Error
             console.log('Error', error.message);
-            }
-            console.log(error.config);
-        }    
+        }
+        console.log(error.config);
+    }    
 }
 
 // export const getWeatherData = async (cityname) => {
