f586249ce7c7f9d69b9b2d81d5019f1812044e42
Gina <Gina@G-Mac.local>
2023-09-15 15:38:28 -0400

    update

--- src/components/TableDaily.js
+++ src/components/TableDaily.js
@@ -2,30 +2,39 @@ import styles from './Table.module.css';
 
 function TableDaily({ list }) {
   console.log(list)
+  const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   return (
     <div>
        {/* <p>{list[0].name}</p>  */}
-       <h2>Daily Weather</h2>
+       <h2>6-Day Forecast</h2>
       <table className={styles.table}>
         <thead>
           <tr className="tableHeader">
-            <th>Time</th>
-            <th>Temp Min</th>
-            <th>Temp Max Probability</th>
-            <th>Humidity Avg</th>
+            <th>Day</th>
+            <th>Temperature</th>
+            <th>Rain</th>
+            <th>Humidity</th>
+            <th>Cloud Cover</th>
+            <th>Wind Speed</th>
           </tr>
         </thead>
          <tbody>
            {list &&
-            list.map((item) => ( 
+            list.map((item) => {
+              const d = new Date(item.time);
+              let day = dayArray[d.getDay()]; 
+              return (
               <tr key={item.id}>
-                <td>{item.time}</td>
-                <td>{item.values.temperatureMin}</td> 
-                <td>{item.values.temperatureMax}</td>  
-                <td>{item.values.humidityAvg}</td>  
-              </tr>
-
-           ))} 
+                {/* <td>{item.time}</td> */}
+                <td>{day}</td>
+                <td>{(item.values.temperatureAvg).toFixed(0) + '\u00b0F'}</td> 
+                <td>{(item.values.rainAccumulationAvg).toFixed(1) + ' in'}</td>  
+                <td>{(item.values.humidityAvg).toFixed(0) + ' g/m3'}</td>  
+                <td>{(item.values.cloudCoverAvg).toFixed(0) + '%'}</td>
+                <td>{(item.values.windSpeedAvg).toFixed(1) + ' mph'}</td>
+             </tr>
+              )
+            })} 
         </tbody> 
       </table>
     </div>
