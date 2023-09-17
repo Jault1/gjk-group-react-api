681bc2917376f5f63f44368b1528011f63ae0c58
John Ault <john.ault.dev@gmail.com>
2023-09-17 09:44:34 -0400

    Adding weather images to table

--- src/components/TableDaily.js
+++ src/components/TableDaily.js
@@ -1,4 +1,8 @@
 import styles from './Table.module.css';
+import img1000 from './../images/icons/1000.png';
+import img1001 from './../images/icons/1001.png';
+import img4000 from './../images/icons/4000.png';
+
 
 function TableDaily({ list, conditions }) {
   console.log(list)
@@ -12,6 +16,7 @@ function TableDaily({ list, conditions }) {
           <tr className="tableHeader">
             <th>Hour</th>
             <th>Conditions</th>
+            <th>Visual conditions</th>
             <th>Temperature</th>
             <th>Humidity</th>
             <th>Wind Speed</th>
@@ -28,6 +33,35 @@ function TableDaily({ list, conditions }) {
                 {/* <td>{item.time}</td> */}
                 <td>{hour}</td>
                 <td>{conditions[code]}</td>
+
+                  <td>
+                  { // sun
+                      ([code] == 1000)
+                      ? <img className="img-fluid" src={img1000} alt="sunny" height='20' /> 
+                      : <span></span> 
+                    }
+                    { // clouds
+                      ([code] == 1001)
+                      ? <img className="img-fluid" src={img1001} alt="cloudy" height='20' /> 
+                      : <span></span> 
+                    }
+                    { //rain
+                      ([code] == 4000)
+                      ? <img className="img-fluid" src={img4000} alt="raining" height='20' /> 
+                      : <span></span> 
+                    }
+                    { //rain
+                      ([code] == 4001)
+                      ? <img className="img-fluid" src={img4000} alt="raining" height='20' /> 
+                      : <span></span> 
+                    }
+    
+                    
+                    
+                    
+                    
+                    </td>
+
                 <td>{(item.values.temperatureAvg).toFixed(0) + '\u00b0 C'}</td> 
                 <td>{(item.values.humidityAvg).toFixed(0) + '%'}</td>  
                 <td>{(item.values.windSpeedAvg).toFixed(1) + ' m/s'}</td>
