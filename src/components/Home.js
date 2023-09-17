c8c6507f8009fa531ef8d672f9fbadcbe8ea5e5c
KPP3 <137243573+KPP3@users.noreply.github.com>
2023-09-17 17:25:21 -0500

    added link to an image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import WorldMap from '../images/WorldMap.png';
+import { Link } from 'react-router-dom';
 
 function Home(){
     return(
     <>
+    <div className="home">
+    <Link to="/search">
    <img src={WorldMap} alt="Map Of Whole World" />
-   </>)
-}
+   </Link>
+   <p><center> Click an image to continue </center></p>
+   </div>
+   </>
+   )}
 
 export default Home
